4af4dc255a97d39fd96cf04d2b1e6661eb619aef
Debbie <debbieforbeck@gmail.com>
2019-05-12 22:10:47 -0400

    added using statements

--- Environment.WebMVC/App_Start/IdentityConfig.cs
+++ Environment.WebMVC/App_Start/IdentityConfig.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using Environment.WebMVC.Models;
+using Environment.Data;
 
 namespace Environment.WebMVC
 {
--- Environment.WebMVC/Controllers/AccountController.cs
+++ Environment.WebMVC/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Environment.WebMVC.Models;
+using Environment.Data;
 
 namespace Environment.WebMVC.Controllers
 {
