faa3046efebbf6353feb579957c2ef0584145f2f
Debbie <debbieforbeck@gmail.com>
2019-06-02 17:22:10 -0400

    started data layer

---
+++ Environment.Data/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Environment.Data
+{
+    class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public string SubBrand { get; set; }
+        public string ProductName { get; set; }
+    }
+}
